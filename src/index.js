429c12cca5252541a3724f798241aefc9f2092c4
EGOIST <0x142857@gmail.com>
2017-03-10 22:48:25 +0800

    fix huge typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from 'src/components/App'
 
-if (process.env.NOD_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   require('./pwa')
 }
 
