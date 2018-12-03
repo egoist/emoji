903f22270d942a7d81313bfbcb4998c8f05960a2
EGOIST <0x142857@gmail.com>
2018-12-03 15:58:48 +0800

    Updates

---
+++ precache-manifest.8e217ab779cc4a1337a9545bb45a2026.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "57ba464fa16e14dbeed0",
+    "url": "/assets/js/3.57ba464f.chunk.js"
+  },
+  {
+    "revision": "7d5bc7bd347b27e66f98d2e1673c387a",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "269093210aba160c924a3373c2135009",
+    "url": "/sw.js"
+  },
+  {
+    "revision": "5d853a0201f45d2beced5081f4ab1270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19ceecf6054f6abe74b5",
+    "url": "/assets/js/runtime~index.19ceecf6.js"
+  },
+  {
+    "revision": "e73eeba0bdc7a23ad4d6",
+    "url": "/assets/js/index.e73eeba0.chunk.js"
+  },
+  {
+    "revision": "907e0627a4da2020cee9",
+    "url": "/assets/js/chunk-vendors.907e0627.chunk.js"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/assets/fonts/node_modules/element-ui/lib/theme-default/fonts/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/assets/fonts/node_modules/element-ui/lib/theme-default/fonts/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "e73eeba0bdc7a23ad4d6",
+    "url": "/assets/css/index.e73eeba0.chunk.css"
+  },
+  {
+    "revision": "907e0627a4da2020cee9",
+    "url": "/assets/css/chunk-vendors.907e0627.chunk.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.8e217ab779cc4a1337a9545bb45a2026.js"
+);
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("index.html", {
+  whitelist: [/^(?!\/__).*/],
+  
+});
+
+workbox.routing.registerRoute(/^https:\/\/twemoji\.maxcdn\.com\//, workbox.strategies.cacheFirst({ plugins: [new workbox.cacheableResponse.Plugin({"statuses":[0,200],"headers":{"content-type":"image/png"}})] }), 'GET');
